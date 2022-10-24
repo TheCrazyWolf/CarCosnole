66f3e852d6f28b68581b0e27a219d266d1df77da
СГК ГАПОУ <sgk.gapou@outlook.com>
2022-10-24 12:59:54 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Car.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Car
+    {
+
+        public string Model; // Модель
+        public int YearBuild; //Год выпуска
+        public const float MaxSpeed = 320; // Макс. скорость
+        public int Run; //Пробег
+
+        public bool StartEngine; // Статус двигателя
+
+        public Vector3 Position;
+
+        public Car()
+        {
+            Position = new Vector3();
+            Model = "Неизвестно";
+            YearBuild = 0000;
+            Run = 0;
+            StartEngine = false;
+        }
+
+        public Car(string model, Vector3 pos)
+        {
+            Position = pos;
+            Model = model;
+            YearBuild = DateTime.Today.Year;
+            Run = 0;
+            StartEngine = false;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine($"Машина {Model} включилась");
+            StartEngine = true;
+        }
+
+        public void Off()
+        {
+            Console.WriteLine($"Машина {Model} выключилась");
+            StartEngine = false;
+        }
+
+        public void Move(Vector3 vector)
+        { 
+            if (!StartEngine)  // (StartEngine != true) or (StartEngine == false)
+                return;
+
+            Position.X += vector.X;
+            Position.Y += vector.Y;
+            Position.Z += vector.Z;
+            Run++;
+
+            Console.WriteLine($"Машина {Model} передвинулась на Позиция = X: {Position.X} Y: {Position.Y} Z: {Position.Z}");
+        }
+
+        public string GetInfo() => $"Тачка {Model}. {YearBuild} года выпуска. Двигатель {StartEngine}. Позиция = X: {Position.X} Y: {Position.Y} Z: {Position.Z}. Пробег {Run}";
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleApp4;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Vector3 vector = new Vector3(1, 2, 3);
+
+        Car audi = new Car("Audi", vector);
+        audi.Start();
+
+        audi.Move(new Vector3(0, 4, 5));
+        Console.WriteLine(audi.GetInfo());
+        audi.Move(new Vector3(0, 4, 5));
+        Console.WriteLine(audi.GetInfo());
+        audi.Off();
+        audi.Move(new Vector3(0, 4, 5));
+        Console.WriteLine(audi.GetInfo());
+        audi.Move(new Vector3(0, 4, 5));
+        Console.WriteLine(audi.GetInfo());
+
+    }
+}
